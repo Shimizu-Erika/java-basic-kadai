5cc7d14653ecab6472a08693e8e2c12bd782a4bb
Shimizu-Erika <eri8170@yahoo.co.jp>
2024-09-06 05:39:54 +0900

    課題提出2

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -1,20 +1,11 @@
 package kadai_021;
 
 public class DictionaryExec_Chapter21 {
-
 	public static void main(String[] args) {
-		//辞書クラスのインスタンス作成		
 		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
 		
-		//調べる英単語を配列にセット
-		dictionary.searchWord("apple");
+		String[] array = {"apple", "banana", "grape", "orange"};
 		
-		dictionary.searchWord("banana");
-		
-		dictionary.searchWord("grape");
-		
-		dictionary.searchWord("orange");
-
+		dictionary.DictionaryExec(array);
 	}
-
 }
--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -2,30 +2,26 @@ package kadai_021;
 
 import java.util.HashMap;
 
-public class Dictionary_Chapter21 {	
-	
-	public void searchWord(String word) {
-	//HashMapの宣言	
-		HashMap<String,String> map = new HashMap<String,String>();
-	
-		//putメソッドで要素を追加
-		map.put("apple","りんご");
-		map.put("peach","桃");
-		map.put("banana","バナナ");
-		map.put("lemon","レモン");
-		map.put("pear","梨");
-		map.put("kiwi","キウイ");
-		map.put("strawberry","いちご");
-		map.put("grape","ぶどう");
-		map.put("muscat","マスカット");
-		map.put("cherry","さくらんぼ");
-	
-		    
-			if (map.containsKey(word)) {
-				System.out.println(word + "の意味は" + map.get(word));
-			}else {
-				System.out.println(word + "は辞書には存在しません");
-			 }
-			
+public class Dictionary_Chapter21 {
+	public void DictionaryExec(String[] array) {
+		HashMap<String,String> englishDictionary = new HashMap<String,String>();
+		englishDictionary.put("apple", "りんご");
+		englishDictionary.put("peach", "桃");
+		englishDictionary.put("banana", "バナナ");
+		englishDictionary.put("lemon", "レモン");
+		englishDictionary.put("pear", "梨");
+		englishDictionary.put("kiwi", "キウィ");
+		englishDictionary.put("strawberry", "いちご");
+		englishDictionary.put("grape", "ぶどう");
+		englishDictionary.put("muscat", "マスカット");
+		englishDictionary.put("cherry", "さくらんぼ");
+		
+		for (String name: array) {
+			if (englishDictionary.containsKey(name)) {
+				System.out.println(name + "の意味は" + englishDictionary.get(name));
+			} else {
+				System.out.println(name + "は辞書に存在しません");
+			}
+		}
 	}
 }
